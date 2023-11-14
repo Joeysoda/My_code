a099df2e435652e50d8f7ebd4ddd1b58e47c09f4
Joeysoda <zj368835@gmail.com>
2023-11-14 22:10:58 +0800

    scrpti

---
+++ script_for_csf/script/a.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+	int i ,j= 0;
+	for (i = 2; i <16 ; i++)
+	{
+		
+			printf("   Or(a=in[%d],out=out[%d]);\n",i,i-2);
+		
+	}
+	return 0;
+}
